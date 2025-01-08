863964b5ea5732d94d8792b8d9e1071b2ce10c83
deepu964 <deepakmv964@gmail.com>
2025-01-08 20:01:38 +0530

    Add files via upload

---
+++ forms/myform.js
@@ -0,0 +1,13 @@
+// function sendMail(){
+//     var params={
+//         name:document.getElementById("name").value,
+        
+//         message:document.getElementById("message").value,
+//         email:document.getElementById("email").value
+//     }
+    
+//     emailjs.send("service_lfkm0ob","template_fuoww48",params).then(function(){
+//         console.log(params);
+//     })
+
+// }
